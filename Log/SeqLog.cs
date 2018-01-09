d58f4a8b9675fc15740f6520522a7f0c74b91b01
SchmidtMichael16 <michaelschmidt@live.at>
2018-01-09 22:00:45 +0100

    add Log Project

---
+++ Log/SeqLog.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using System;
+
+namespace Log
+{
+    public static class SeqLog
+    {
+        public static void WriteNewLogMessage(string message, params object[] propertyValues)
+        {
+            Serilog.Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+
+            Serilog.Log.Information(message, propertyValues);
+
+            // Important to call at exit so that batched events are flushed.
+            Serilog.Log.CloseAndFlush();
+
+        }
+
+    }
+}
